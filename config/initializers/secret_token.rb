fb951113fce9e9521a7555bf067a616614b7e6a4
Lê-Van Hoang <levanh12@gmail.com>
2014-03-30 16:00:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CvTuesday::Application.config.secret_key_base = 'e2e00c2635cb524c1666e3e0ef3d0d8a5174f1b21f01333722c93796be69b3331376403c332eda101ded09cab2f782608ac786eb50faef0484f4d9edd133c68e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CvTuesday::Application.config.session_store :cookie_store, key: '_cv_tuesday_session'
